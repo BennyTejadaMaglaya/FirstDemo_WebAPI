4a3e04b924f9ae898b2efc425f9acf3770d86013
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-02-11 06:32:56 -0500

    Make copies of the models, then rename them to ___DTO.cs.

---
+++ FirstDemo_WebAPI/Models/DoctorDTO.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace FirstDemo_WebAPI.Models
+{
+    [ModelMetadataType(typeof(DoctorMetaData))]
+    public class DoctorDTO
+    {
+        public int ID { get; set; }
+
+        //[JsonIgnore]
+        public string Summary
+        {
+            get
+            {
+                return "Dr. " + FirstName
+                    + (string.IsNullOrEmpty(MiddleName) ? " " :
+                        (" " + (char?)MiddleName[0] + ". ").ToUpper())
+                    + LastName;
+            }
+        }
+
+        //[JsonIgnore]
+        public string FormalName
+        {
+            get
+            {
+                return LastName + ", " + FirstName
+                    + (string.IsNullOrEmpty(MiddleName) ? "" :
+                        (" " + (char?)MiddleName[0] + ".").ToUpper());
+            }
+        }
+
+        public string FirstName { get; set; } = "";
+
+        public string? MiddleName { get; set; }
+
+        public string LastName { get; set; } = "";
+
+        public Byte[]? RowVersion { get; set; }
+
+        //[JsonIgnore]
+        public ICollection<Patient> Patients { get; set; } = new HashSet<Patient>();
+    }
+}
---
+++ FirstDemo_WebAPI/Models/PatientDTO.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace FirstDemo_WebAPI.Models
+{
+    [ModelMetadataType(typeof(PatientMetaData))]
+    public class PatientDTO : IValidatableObject
+    {
+        public int ID { get; set; }
+
+        public string Summary
+        {
+            get
+            {
+                return FirstName
+                    + (string.IsNullOrEmpty(MiddleName) ? " " :
+                        (" " + (char?)MiddleName[0] + ". ").ToUpper())
+                    + LastName;
+            }
+        }
+
+        public string FirstName { get; set; } = "";
+
+        public string? MiddleName { get; set; }
+
+        public string LastName { get; set; } = "";
+
+        public string OHIP { get; set; } = "";
+
+        public DateTime DOB { get; set; }
+
+        public byte ExpYrVisits { get; set; } = 2;//Most common value is 2.
+
+        public Byte[]? RowVersion { get; set; }
+
+        public int DoctorID { get; set; }
+        public Doctor? Doctor { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (DOB > DateTime.Today.AddDays(1))
+            {
+                yield return new ValidationResult("Date of Birth cannot be in the future.", new[] { "DOB" });
+            }
+        }
+    }
+}
