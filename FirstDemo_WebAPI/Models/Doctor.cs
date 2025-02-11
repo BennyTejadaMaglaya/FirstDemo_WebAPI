95aea3fff838d4b33a458f778e678e2cd57a245d
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-02-11 06:27:10 -0500

    25.	Add [ModelMetadataType(typeof(___MetaData))] on top and remove all annotations.

--- FirstDemo_WebAPI/Models/Doctor.cs
+++ FirstDemo_WebAPI/Models/Doctor.cs
@@ -1,13 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 
 namespace FirstDemo_WebAPI.Models
 {
+    [ModelMetadataType(typeof(DoctorMetaData))]
     public class Doctor
     {
         public int ID { get; set; }
 
-        [Display(Name = "Doctor")]
         //[JsonIgnore]
         public string Summary
         {
@@ -31,21 +33,12 @@ namespace FirstDemo_WebAPI.Models
             }
         }
 
-        [Display(Name = "First Name")]
-        [Required(ErrorMessage = "The Doctor's first name cannot be left blank.")]
-        [StringLength(50, ErrorMessage = "First name cannot be more than 50 characters long.")]
         public string FirstName { get; set; } = "";
 
-        [Display(Name = "Middle Name")]
-        [StringLength(50, ErrorMessage = "Middle name cannot be more than 50 characters long.")]
         public string? MiddleName { get; set; }
 
-        [Display(Name = "Last Name")]
-        [Required(ErrorMessage = "The Doctor's last name is required.")]
-        [StringLength(100, ErrorMessage = "Last name cannot be more than 100 characters long.")]
         public string LastName { get; set; } = "";
 
-        [Timestamp]
         public Byte[]? RowVersion { get; set; }
 
         //[JsonIgnore]
