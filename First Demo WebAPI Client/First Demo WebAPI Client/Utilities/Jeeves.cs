c9a2c26390e11f104d9c5b0a0704e7edda2629bf
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-02-20 15:11:15 -0500

    In Utilities, add a class ApiException.

--- First Demo WebAPI Client/First Demo WebAPI Client/Utilities/Jeeves.cs
+++ First Demo WebAPI Client/First Demo WebAPI Client/Utilities/Jeeves.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Windows.UI.Xaml.Controls;
 
 namespace First_Demo_WebAPI_Client.Utilities
@@ -42,5 +44,37 @@ namespace First_Demo_WebAPI_Client.Utilities
             return result;
         }
 
+        public static ApiException CreateApiException(HttpResponseMessage response)
+        {
+            var httpErrorObject = response.Content.ReadAsStringAsync().Result;
+
+            // Create an anonymous object to use as the template for deserialization:
+            var anonymousErrorObject =
+                new { message = "", errors = new Dictionary<string, string[]>() };
+
+            // Deserialize:
+            var deserializedErrorObject =
+                JsonConvert.DeserializeAnonymousType(httpErrorObject, anonymousErrorObject);
+
+            // Now wrap into an exception which best fullfills the needs of your application:
+            var ex = new ApiException(response);
+
+            //Check for a message
+            if (deserializedErrorObject?.message != null)
+            {
+                ex.Data.Add(-1, deserializedErrorObject?.message);
+            }
+            // Or sometimes, there may be Model Errors:
+            if (deserializedErrorObject.errors != null)
+            {
+                foreach (var err in deserializedErrorObject?.errors)
+                {
+                    //Note that we only want the first error message
+                    //string for a "key" becuase it is the one we created
+                    ex.Data.Add(err.Key, err.Value[0]);
+                }
+            }
+            return ex;
+        }
     }
 }
